b59cf3844cfbf8f8aae142289815f781113a0525
kokoruz <kokoruz@comcast.net>
2018-02-28 13:59:02 -0600

    Added Browsify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
+    
 
 var coffeeSources = [ 'components/coffee/tagline.coffee'];
 var jsSources = [
@@ -21,5 +23,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('scripts.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js')) 
 });
